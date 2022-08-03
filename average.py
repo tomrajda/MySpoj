54d876e96170e82469542f5eb6faa214c93a856a
tomrajda <eu.crafciarze@gmail.com>
2022-08-03 19:30:13 +0200

    Update 03.08.2022

--- average.py
+++ average.py
@@ -1,14 +1,22 @@
-iterations = int(input())
+def closest(nums):
+    #calculate average of input numbers
+    int_nums = [int(num) for num in nums.split()]
+    qua = int_nums.pop(0)
+    avg = sum(int_nums) / qua
+    res = max(int_nums)
 
-if iterations < 101 and iterations > 0:
-    for _ in range(1, iterations+1):
-        sum = 0
-        numbers = input().split()
+    #looking for closest number to avg
+    for int_num in int_nums:
+        dif = abs(avg - int_num)
+        if dif < res:
+            clo = int_num
+            res = dif
+        if res == 0:
+            print(int_num)
+            return None
+    print(clo)
 
-        if all(int(i) < 100 for i in numbers):
-            for number in numbers:
-                sum += int(number)
-            res = sum / len(numbers)
-            print(round(res))
-        else:
-            break
+for _ in range(int(input())):
+    closest(input())
+
+#https://pl.spoj.com/problems/PP0604A/
