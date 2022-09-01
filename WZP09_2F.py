808bae12ed3e939d20c268459f12081458260b86
tomrajda <eu.crafciarze@gmail.com>
2022-09-01 21:42:09 +0200

    Update 01.09.2022

---
+++ WZP09_2F.py
@@ -0,0 +1,10 @@
+inpt = ""
+while inpt != 0:
+    inpt = int(input())
+    if inpt != 0:
+        if inpt % 15 == 0:
+            print("TAK")
+        else:
+            print("NIE")
+
+#https://pl.spoj.com/problems/WZP09_2F/
